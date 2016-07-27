1d1e0ef051bc47d5f200ff654cbd87f88f998e81
Dietrich <dietricherwane@yahoo.fr>
2016-07-27 14:46:39 +0000

    changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -27,25 +27,39 @@ class HomeController < ApplicationController
   end
 
   def create_parionsdirect_account
-    pseudo = params[:pseudo]
-    firstname = params[:firstname]
-    lastname = params[:lastname]
-    email = params[:email]
-    password = params[:password]
-    password_confirmation = params[:password_confirmation]
-    birthdate = params[:birthdate]
+    @pseudo = params[:pseudo]
+    @firstname = params[:firstname]
+    @lastname = params[:lastname]
+    @email = params[:email]
+    @password = params[:password]
+    @password_confirmation = params[:password_confirmation]
+    @birthdate = params[:birthdate]
 
-    url = Parameter.first.gateway_url + "/6ba041bf35229938ba869a7a9c59f3a0/api/users/account/create/1/1/#{pseudo}/#{firstname}/#{lastname}/#{email}/#{password}/#{password_confirmation}/#{session[:msisdn]}/birthdate/2"
+    if valid_parionsdirect_account_params?
+      url = Parameter.first.gateway_url + "/6ba041bf35229938ba869a7a9c59f3a0/api/users/account/create/1/1/#{@pseudo}/#{@firstname}/#{@lastname}/#{@email}/#{@password}/#{@password_confirmation}/#{session[:msisdn]}/birthdate/2"
 
-    parionsdirect_account = RestClient.get(url) rescue nil
+      parionsdirect_account = RestClient.get(url) rescue nil
 
-    GenericLog.create(operation: "Create parionsdirect account", request_log: url, response_log: parionsdirect_account)
+      GenericLog.create(operation: "Create parionsdirect account", request_log: url, response_log: parionsdirect_account)
 
-    parionsdirect_account = JSON.parse(parionsdirect_account) rescue nil
+      parionsdirect_account = JSON.parse(parionsdirect_account) rescue nil
 
-    flash.now[:error] = parionsdirect_account.to_s
+      flash.now[:error] = parionsdirect_account.to_s
+    else
+      flash.now[:error] = 'Veuillez renseigner tous les paramètres.'
+    end
 
     render :new_parionsdirect_account
+  end
+
+  # Check parionsdirect creation parameters
+  def valid_parionsdirect_account_params?
+    status = true
+    if @pseudo.blank? || @firstname.blank? || @lastname.blank? || @email.blank? || @password.blank? || @password_confirmation.blank? || @birthdate.blank?
+      status = false
+    end
+
+    return status
   end
 
   def parionsdirect_authentication_form
