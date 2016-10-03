83bd54a9d38b2098a86fc242412a09548260c46f
Dietrich <dietricherwane@yahoo.fr>
2016-10-03 12:39:01 +0000

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -77,9 +77,6 @@ Rails.application.routes.draw do
 
   wash_out :ussd_testing
 
-  # MTN ussd
-  #wash_out :rumbas
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
