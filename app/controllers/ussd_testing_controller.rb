11c2e1914f9450e3196b1fdcafdaa5a5d51f00c4
Dietrich <dietricherwane@yahoo.fr>
2016-10-09 10:31:19 +0000

    changes

--- app/controllers/ussd_testing_controller.rb
+++ app/controllers/ussd_testing_controller.rb
@@ -141,7 +141,7 @@ class UssdTestingController < ApplicationController
       c_main_menu_check_service_code
       c_main_menu_check_ussd_string
 
-      UssdReceptionLog.create(received_parameters: @raw_body, rev_id: @rev_id, rev_password: @rev_password, sp_id: @sp_id, service_id: @service_id, timestamp: @timestamp, trace_unique_id: @unique_id, msg_type: @msg_type, sender_cb: @sender_cb, receiver_cb: @receive_cb, ussd_of_type: @ussd_op_type, msisdn: @msisdn, service_code: @service_code, service_code: , code_scheme: @code_scheme, ussd_string: @ussd_string, error_code: @error_code, error_message: @error_message, remote_ip: remote_ip_address)
+      UssdReceptionLog.create(received_parameters: @raw_body, rev_id: @rev_id, rev_password: @rev_password, sp_id: @sp_id, service_id: @service_id, timestamp: @timestamp, trace_unique_id: @unique_id, msg_type: @msg_type, sender_cb: @sender_cb, receiver_cb: @receive_cb, ussd_of_type: @ussd_op_type, msisdn: @msisdn, service_code: @service_code, code_scheme: @code_scheme, ussd_string: @ussd_string, error_code: @error_code, error_message: @error_message, remote_ip: remote_ip_address)
     end
 
     result = %Q[
