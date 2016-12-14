cb84b924b02cfa7f4cb346e11fb8a225af901ead
Dietrich <dietricherwane@yahoo.fr>
2016-12-14 18:25:08 +0000

    changes

--- app/controllers/ussd_testing_controller.rb
+++ app/controllers/ussd_testing_controller.rb
@@ -4747,8 +4747,8 @@ Saisissez le montant du rechargement
       @reload_request = "http://41.189.40.193:6968/MTNCI/ussd/reload/8f90aaece362b6d83b6887cc19067433/75592949-2b13-4175-b811-3caf75687355/#{Digest::SHA1.hexdigest([DateTime.now.iso8601(6), rand].join).hex.to_s[0..8]}/22545349536/#{@ussd_string}/XOF/#{@current_ussd_session.reload_account.blank? ? AccountProfile.find_by_msisdn(@msisdn[-8,8]).paymoney_account_number : @current_ussd_session.reload_account}"
       @reload_response = RestClient.get(@reload_request) rescue ''
 
-      if @reload_response == '1'
-        @rendered_text = %Q[Votre compte de jeu a été recharge avec succes. Montant : #{@ussd_string} FCFA.]
+      if @reload_response == '2'
+        @rendered_text = %Q[Votre demande de rechargement est en cours de traitement. Montant : #{@ussd_string} FCFA.]
         @session_identifier = '7--'
       else
         @rendered_text = %Q[La transaction a échoué, Veuillez réessayer
@@ -4788,8 +4788,8 @@ Saisissez le montant du déchargement
       @unload_request = "http://41.189.40.193:6968/MTNCI/ussd/unload/8f90aaece362b6d83b6887cc19067433/75592949-2b13-4175-b811-3caf75687002/#{Digest::SHA1.hexdigest([DateTime.now.iso8601(6), rand].join).hex.to_s[0..8]}/22545349536/#{@current_ussd_session.unload_amount}/XOF/#{@current_ussd_session.unload_account.blank? ? AccountProfile.find_by_msisdn(@msisdn[-8,8]).paymoney_account_number : @current_ussd_session.unload_account}/#{@ussd_string}"
       @unload_response = RestClient.get(@unload_request) rescue ''
 
-      if @unload_response == '2'
-        @rendered_text = %Q[Votre rechargement est en cours de traitement. Montant : #{@current_ussd_session.unload_amount} FCFA.]
+      if @unload_response == '1'
+        @rendered_text = %Q[Votre transaction a été effectuée avec succès. Montant : #{@current_ussd_session.unload_amount} FCFA.]
         @session_identifier = '11--'
       else
         @rendered_text = %Q[La transaction a échoué, Veuillez réessayer
