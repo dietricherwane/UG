8cf2d8f50bbf6e525ab7077804a2b28191661c78
Dietrich <dietricherwane@yahoo.fr>
2016-12-21 18:06:25 +0000

    changes

--- app/controllers/ussd_testing_controller.rb
+++ app/controllers/ussd_testing_controller.rb
@@ -754,7 +754,7 @@ Faites vos pronostics. Choisissez votre pari :
 
     UssdReceptionLog.create(received_parameters: @raw_body, rev_id: @rev_id, rev_password: @rev_password, sp_id: @sp_id, service_id: @service_id, timestamp: @timestamp, trace_unique_id: @unique_id, msg_type: @msg_type, sender_cb: @sender_cb, receiver_cb: @receive_cb, ussd_of_type: @ussd_op_type, msisdn: @msisdn, service_code: @service_code, code_scheme: @code_scheme, ussd_string: @ussd_string, error_code: @error_code, error_message: @error_message, remote_ip: remote_ip_address)
 
-    @account_profile = AccountProfile.find_by_msisdn(@msisdn[-8,8])
+    @account_profile = AccountProfile.find_by_msisdn(@msisdn[-8,8]) rescue nil
 
     render :xml => @result
 
